899a009a7f22cdb39356d0f67ca0d535c207651a
mlebrun <matthieulebrun55@gmail.com>
2022-12-30 14:23:24 +0100

    feat: create pong menu

--- front/src/components/Canvas.tsx
+++ front/src/components/Canvas.tsx
@@ -18,19 +18,108 @@ export default function Canvas()
 
 	useEffect(() =>
 	{
-		ctx.current = canvasRef.current.getContext("2d");
-		
-		playerA.current = new Player(75, 100, size.current.width / 60, size.current.height / 5, 2, "white", ctx.current);
-		playerB.current = new Player(size.current.width - 100, size.current.height - 100, size.current.width / 60, size.current.height / 5, 2, "white", ctx.current);
+		interface TextZone{
+			posX : number;
+			posY : number;
+			width : number;
+			height : number;
+		}
 
-		ball.current = new Ball(size.current.width / 2, size.current.height / 2, 10, "white", ctx.current!);
+		function addLink(textZone : TextZone, linkAction : Function)
+		{
+			var canvas = document.getElementById('canvas');
+			var	mouseOnZone : TextZone | null = null;
+			var executeLink = (e : MouseEvent) =>
+			{
 
-		socket.current.on("ball", (arg : string) => {
-		ball.current!.update_pos(JSON.parse(arg).current)});
-		socket.current.on("playerA", (arg : string) => {
-		playerA.current!.update_pos(JSON.parse(arg).current)});
-		socket.current.on("playerB", (arg : string) => {
-		playerB.current!.update_pos(JSON.parse(arg).current);});
+				var clickZone = canvas!.getBoundingClientRect();
+
+				if (e.clientX - clickZone.left >= textZone.posX &&
+				e.clientX - clickZone.left <= textZone.posX + textZone.width)
+				{
+					if (e.clientY - clickZone.top >= textZone.posY &&
+					e.clientY - clickZone.top <= textZone.posY + textZone.height)
+					{
+						canvas!.removeEventListener('click', executeLink);
+						canvas!.removeEventListener('mousemove', drawMousePointer);
+						canvas!.style.cursor = 'default';
+						linkAction();
+					}
+				}
+			};
+			var drawMousePointer = (e : MouseEvent) =>
+			{
+				var clickZone = canvas!.getBoundingClientRect();
+
+				if (e.clientX - clickZone.left >= textZone.posX &&
+				e.clientX - clickZone.left <= textZone.posX + textZone.width)
+				{
+					if (e.clientY - clickZone.top >= textZone.posY &&
+					e.clientY - clickZone.top <= textZone.posY + textZone.height)
+					{
+						canvas!.style.cursor = 'pointer';
+						mouseOnZone = textZone;
+					}
+					else
+					{
+						if (mouseOnZone && mouseOnZone === textZone)
+							canvas!.style.cursor = 'default';
+					}
+				}
+				else
+				{
+						if (mouseOnZone && mouseOnZone === textZone)
+							canvas!.style.cursor = 'default';
+				}
+			}
+			canvas!.addEventListener('click', executeLink)
+			canvas!.addEventListener('mousemove', drawMousePointer);
+		}
+
+		function getTextZone(text: string, posX : number, posY : number)
+		{
+			let	gamezone : TextMetrics = ctx.current!.measureText(text);
+
+			return ({posX : posX - gamezone.actualBoundingBoxLeft, posY : posY - gamezone.fontBoundingBoxAscent,
+			height: gamezone.fontBoundingBoxAscent + gamezone.fontBoundingBoxDescent,
+			width : gamezone.width});
+		}
+
+		function drawText(text : string, posX : number, posY : number, size : number)
+		{
+			let	textZone : TextZone | undefined;
+
+			ctx.current!.beginPath();
+			ctx.current!.fillStyle = "pink";
+			ctx.current!.font = size + "px Courier New";
+			ctx.current!.textAlign = 'center';
+			ctx.current!.fillText(text, posX, posY);
+			textZone = getTextZone(text, posX, posY);
+			return (textZone);
+		}
+
+		function drawMatchmaking()
+		{
+			ctx.current!.fillStyle = 'black';
+			ctx.current!.fillRect(0, 0, size.current.width, size.current.height);
+			drawText("looking for player...", size.current.width / 2, size.current.height / 2, 35);
+		}
+
+		function drawMenu()
+		{
+			ctx.current!.fillStyle = 'black';
+			ctx.current!.fillRect(0, 0, size.current.width, size.current.height);
+
+			let newGameZone = drawText("new game", size.current.width / 2, size.current.height / 2, 35);
+			let skinsZone = drawText("skins", size.current.width / 4, size.current.height / 1.3, 20);
+			let mapsZone = drawText("maps", size.current.width / 1.3, size.current.height / 1.3, 20);
+			//let menuElem = [newGameZone, skinsZone, mapsZone];
+//			ctx.current!.fillStyle = "rgba(255, 0, 0, 0.5)";
+//			ctx.current!.fillRect(newGameZone.posX, newGameZone.posY, newGameZone.width, newGameZone.height);
+//			console.log("width = ", newGameZone!.width);
+			addLink(newGameZone, drawMatchmaking);
+//			menuElem.map((zone) => {addLink(zone!)})
+		}
 
 		function draw()
 		{
@@ -57,6 +146,21 @@ export default function Canvas()
 			draw();
 			animationFrameId.current = window.requestAnimationFrame(render)
 		}
+
+		ctx.current = canvasRef.current.getContext("2d");
+		drawMenu();
+		playerA.current = new Player(75, 100, size.current.width / 60, size.current.height / 5, 4, "white", ctx.current);
+		playerB.current = new Player(size.current.width - 100, size.current.height - 100, size.current.width / 60, size.current.height / 5, 4, "white", ctx.current);
+
+		ball.current = new Ball(size.current.width / 2, size.current.height / 2, 10, "white", ctx.current!);
+
+		socket.current.on("ball", (arg : string) => {
+		ball.current!.update_pos(JSON.parse(arg).current)});
+		socket.current.on("playerA", (arg : string) => {
+		playerA.current!.update_pos(JSON.parse(arg).current)});
+		socket.current.on("playerB", (arg : string) => {
+		playerB.current!.update_pos(JSON.parse(arg).current);});
+
 
 		kd.current.W.down(function()
 		{
@@ -86,10 +190,9 @@ export default function Canvas()
 				kd.current.tick();
 		});
 
-		render();
 		return () => { window.cancelAnimationFrame(animationFrameId.current) }
 
 	}, []);
 
-	return (<center><canvas style={{marginTop: 150}} width={size.current.width} height={size.current.height} ref={canvasRef}></canvas></center>);
+	return (<center><canvas id="canvas" style={{marginTop: 150}} width={size.current.width} height={size.current.height} ref={canvasRef}></canvas></center>);
 }
