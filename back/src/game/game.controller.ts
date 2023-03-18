2074e29c806ae80038cbada6d02f3cba7440c42c
mlebrun <matthieulebrun55@gmail.com>
2023-03-18 01:20:15 +0100

    fix: secure spectate games requests

--- back/src/game/game.controller.ts
+++ back/src/game/game.controller.ts
@@ -1,17 +1,10 @@
-import { Controller, Get, Param} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GameService } from './game.service';
 
 @Controller('game')
 class GameController
 {
 	constructor(private gameService: GameService) {}
-
-	@Get(':id')
-	getGame(@Param('id') id: string)
-	{
-		console.log('id = ', id);
-		return ("");
-	}
 
 	@Get()
 	async getGames()
