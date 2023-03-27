bd974d56a73deccee0f1b0919834df07ffa295a8
mlebrun <matthieulebrun55@gmail.com>
2023-03-27 23:21:13 +0200

    fix: user status on signup with 42's api

--- front/src/pages/signin/components/Callback.tsx
+++ front/src/pages/signin/components/Callback.tsx
@@ -1,13 +1,15 @@
 import { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { axiosMain } from "src/api/axios";
 import useQuery from "../../useQuery";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Loading from "src/pages/Loading";
 import { useSnackbar } from "notistack";
 import VerifTfa from "src/pages/settings/components/VerifTfa";
 import { ResponseInterface, SignInterface } from "src/interfaces/Sign";
 import Headers from "src/components/Headers";
+import { SocketContext } from '../../../context/SocketContext';
+import { getToken } from '../../../api/axios';
 
 const CALLBACK_PATH = "/auth/signin/42login/callback";
 
@@ -18,6 +20,7 @@ const Callback = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isTfa, setIsTfa] = useState<boolean>(false);
   const [TfaSuccess, setTfaSuccess] = useState<boolean>(false);
+  const socket = useContext(SocketContext);
   const [resp, setResp] = useState<ResponseInterface>({
     tokens: undefined,
     id: undefined,
@@ -45,6 +48,8 @@ const Callback = () => {
       }
         setUsername(response.data.username);
         setIsTfa(response.data.isTfa);
+	  	socket.auth = {token: getToken().access_token}
+	  	socket.connect();
       } catch (err: any) {
         if (!err?.response) {
           setErrMsg("No Server Response");
