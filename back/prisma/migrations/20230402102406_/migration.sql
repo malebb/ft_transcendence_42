16f6ba36d78b646684b2ced5def0f711e2845964
Liena DERMIGNY <ldermign@studentx.42.fr>
2023-04-02 12:29:19 +0200

    fix: trim username in chatroom and private message

---
+++ back/prisma/migrations/20230402102406_/migration.sql
@@ -0,0 +1,294 @@
+-- CreateEnum
+CREATE TYPE "Activity" AS ENUM ('ONLINE', 'OFFLINE', 'IN_GAME');
+
+-- CreateEnum
+CREATE TYPE "MessageType" AS ENUM ('STANDARD', 'INVITATION');
+
+-- CreateEnum
+CREATE TYPE "Accessibility" AS ENUM ('PUBLIC', 'PRIVATE', 'PROTECTED', 'PRIVATE_PROTECTED');
+
+-- CreateEnum
+CREATE TYPE "PenaltyType" AS ENUM ('BAN', 'MUTE');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pending', 'accepted', 'declined');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "id42" TEXT,
+    "username" TEXT NOT NULL,
+    "status" "Activity" NOT NULL DEFAULT 'OFFLINE',
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "hashRt" TEXT[],
+    "TFA" TEXT,
+    "isTFA" BOOLEAN NOT NULL DEFAULT false,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "profilePicture" TEXT NOT NULL,
+    "skin" TEXT NOT NULL DEFAULT 'white',
+    "map" TEXT NOT NULL DEFAULT 'basic',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Challenge" (
+    "id" SERIAL NOT NULL,
+    "powerUpMode" BOOLEAN NOT NULL,
+    "receiverId" INTEGER NOT NULL,
+    "senderId" INTEGER NOT NULL,
+
+    CONSTRAINT "Challenge_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChatRoom" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT,
+    "accessibility" "Accessibility" NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+
+    CONSTRAINT "ChatRoom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "message" TEXT NOT NULL,
+    "chatRoomId" INTEGER,
+    "sendAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "privateMessageId" INTEGER,
+    "challengeId" INTEGER,
+    "type" "MessageType" NOT NULL DEFAULT 'STANDARD',
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PrivateMessage" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'PRIVATE',
+    "senderId" INTEGER NOT NULL,
+    "receiverId" INTEGER NOT NULL,
+
+    CONSTRAINT "PrivateMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Penalty" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "chatId" INTEGER NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "targetId" INTEGER NOT NULL,
+    "type" "PenaltyType" NOT NULL,
+    "durationInMin" INTEGER NOT NULL,
+
+    CONSTRAINT "Penalty_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FriendRequest" (
+    "id" SERIAL NOT NULL,
+    "status" "Status" NOT NULL,
+    "creatorId" INTEGER NOT NULL,
+    "receiverId" INTEGER NOT NULL,
+
+    CONSTRAINT "FriendRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Stats" (
+    "id" SERIAL NOT NULL,
+    "defeat" INTEGER NOT NULL DEFAULT 0,
+    "victory" INTEGER NOT NULL DEFAULT 0,
+    "xp" INTEGER NOT NULL DEFAULT 0,
+    "level" INTEGER NOT NULL DEFAULT 0,
+    "modeExplorer" BOOLEAN NOT NULL DEFAULT false,
+    "fashionWeek" BOOLEAN NOT NULL DEFAULT false,
+    "traveler" BOOLEAN NOT NULL DEFAULT false,
+    "failureKnowledge" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Stats_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GamePlayed" (
+    "id" SERIAL NOT NULL,
+    "leftPlayerId" INTEGER NOT NULL,
+    "rightPlayerId" INTEGER NOT NULL,
+    "leftScore" INTEGER NOT NULL,
+    "rightScore" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "GamePlayed_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AchievementDone" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "AchievementDone_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "leftPlayerId" INTEGER NOT NULL,
+    "rightPlayerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_Blocked" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_UserAdmin" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_UserMember" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_GamePlayedToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id42_key" ON "users"("id42");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChatRoom_name_key" ON "ChatRoom"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Stats_userId_key" ON "Stats"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Game_gameId_key" ON "Game"("gameId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_Blocked_AB_unique" ON "_Blocked"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Blocked_B_index" ON "_Blocked"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserAdmin_AB_unique" ON "_UserAdmin"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserAdmin_B_index" ON "_UserAdmin"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserMember_AB_unique" ON "_UserMember"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserMember_B_index" ON "_UserMember"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_GamePlayedToUser_AB_unique" ON "_GamePlayedToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_GamePlayedToUser_B_index" ON "_GamePlayedToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "Challenge" ADD CONSTRAINT "Challenge_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Challenge" ADD CONSTRAINT "Challenge_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatRoom" ADD CONSTRAINT "ChatRoom_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatRoomId_fkey" FOREIGN KEY ("chatRoomId") REFERENCES "ChatRoom"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_privateMessageId_fkey" FOREIGN KEY ("privateMessageId") REFERENCES "PrivateMessage"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PrivateMessage" ADD CONSTRAINT "PrivateMessage_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PrivateMessage" ADD CONSTRAINT "PrivateMessage_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Penalty" ADD CONSTRAINT "Penalty_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "ChatRoom"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Penalty" ADD CONSTRAINT "Penalty_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Penalty" ADD CONSTRAINT "Penalty_targetId_fkey" FOREIGN KEY ("targetId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Stats" ADD CONSTRAINT "Stats_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GamePlayed" ADD CONSTRAINT "GamePlayed_leftPlayerId_fkey" FOREIGN KEY ("leftPlayerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GamePlayed" ADD CONSTRAINT "GamePlayed_rightPlayerId_fkey" FOREIGN KEY ("rightPlayerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AchievementDone" ADD CONSTRAINT "AchievementDone_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_leftPlayerId_fkey" FOREIGN KEY ("leftPlayerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_rightPlayerId_fkey" FOREIGN KEY ("rightPlayerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Blocked" ADD CONSTRAINT "_Blocked_A_fkey" FOREIGN KEY ("A") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Blocked" ADD CONSTRAINT "_Blocked_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserAdmin" ADD CONSTRAINT "_UserAdmin_A_fkey" FOREIGN KEY ("A") REFERENCES "ChatRoom"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserAdmin" ADD CONSTRAINT "_UserAdmin_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserMember" ADD CONSTRAINT "_UserMember_A_fkey" FOREIGN KEY ("A") REFERENCES "ChatRoom"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserMember" ADD CONSTRAINT "_UserMember_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GamePlayedToUser" ADD CONSTRAINT "_GamePlayedToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "GamePlayed"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GamePlayedToUser" ADD CONSTRAINT "_GamePlayedToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
